1fed44ad8361036d2f2fcf6848175dac788fad24
vacya01 <55055723+vacya01@users.noreply.github.com>
2021-04-03 15:25:07 +0500

    Error page added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, abort
 from flask_login import logout_user, login_required, LoginManager, login_user, current_user
 
 from data import db_session
@@ -13,7 +13,7 @@ login_manager.init_app(app)
 
 
 def main():
-    db_session.global_init('db/database.sqlite')
+    db_session.global_init('C:/Users/Vasiliy/PycharmProjects/edu-area/db/database.db')
     app.run(debug=True)
 
 
@@ -87,7 +87,7 @@ def load_user(user_id):
 @app.route('/homework')
 def homework_page():
     if not current_user.is_authenticated:
-        return redirect('/login')
+        return abort(401)
     subjects = [
         ['Информатика', 'Физика', 'Геометрия', 'ОБЖ'],
         ['Английский язык', 'Физика', 'Литература'],
@@ -103,13 +103,26 @@ def homework_page():
         [],
         [],
         []
-                ]
+    ]
     days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
     time_start = ['9:00', '9:55', '10:50', '11:55', '13:00', '13:55', '14:50', '15:45']
     time_end = ['9:45', '10:40', '11:35', '12:40', '13:45', '14:40', '15:35', '16:30']
     return render_template('homework.html', title='Дневник', subjects=subjects,
                            homework=homework, days=days,
                            time_start=time_start, time_end=time_end)
+
+
+@app.errorhandler(401)
+@app.errorhandler(404)
+def page_not_found(error):
+    messages = {
+        401: ['Вы не авторизованы', 'Через 5 секунд Вы будете направлены на страницу авторизации'],
+        404: ['Страница не найдена', 'Проверьте правильность введённого адреса']
+    }
+    return render_template("error-page.html",
+                           code=error.code,
+                           title=messages[error.code][0],
+                           message=messages[error.code][1])
 
 
 if __name__ == '__main__':
