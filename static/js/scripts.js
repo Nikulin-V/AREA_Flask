f167cd7c56c01418e6cdbe1fed98ec19c7311cb8
vacya01 <55055723+vacya01@users.noreply.github.com>
2021-04-07 16:25:13 +0500

    Added explanatory note and presentation Added light-footer.css

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -15,9 +15,9 @@ let hostUrl = window.location.pathname;
 let options = 'fixed left 29%'
 let main = document.getElementsByTagName('body')[0]
 if(hostUrl === '/login' || hostUrl === '/register'){
-    main.style.background = "#fff url(\"/static/images/Обои.jpg\") " + options;
+    main.style.background = "#fff url(\"/static/images/.jpg\") " + options;
 }
 if(hostUrl === '/'){
-    main.style.background = "#fff url(\"/static/images/Обои.jpg\") " + options;
+    main.style.background = "#fff url(\"/static/images/.jpg\") " + options;
 }
 main.style.backgroundSize = 'auto 80%'
