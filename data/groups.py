d84af60e88c4ac017185455a9f4cebaa7bd506d3
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2021-05-02 00:15:57 +0500

    Students table added AREA-23 #comment Готово

--- data/groups.py
+++ data/groups.py
@@ -8,7 +8,5 @@ class Groups(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     subject = sqlalchemy.Column(sqlalchemy.String)
-    school = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("schools.id"))
-    school_class = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("classes.id"))
     teacher_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     students_ids = sqlalchemy.Column(sqlalchemy.String)
