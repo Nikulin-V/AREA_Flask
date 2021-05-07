2fff4f17ba0a565297061fee9099c2567e07e434
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2021-05-07 22:56:49 +0500

    AREA-33 Добавлена таблица домашних заданий

--- data/homeworks.py
+++ data/homeworks.py
@@ -10,3 +10,4 @@ class Homework(SqlAlchemyBase):
     group_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("groups.id"))
     date = sqlalchemy.Column(sqlalchemy.Date)
     homework = sqlalchemy.Column(sqlalchemy.String)
+    marks = sqlalchemy.Column(sqlalchemy.String)
