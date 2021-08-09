c66be9773c729c802965aba1714637575f806821
Василий <vacya01@users.noreply.github.com>
2021-08-09 12:44:51 +0500

    Added fee for deals

--- api/offers.py
+++ api/offers.py
@@ -6,7 +6,7 @@ from flask_login import current_user, login_required
 from api import api, sock
 from data import db_session
 from data.companies import Company
-from data.functions import get_session_id, get_company_title, get_company_id
+from data.functions import get_session_id, get_company_title, get_company_id, get_constant
 from data.offers import Offer
 from data.scheduled_job import ScheduledJob
 from data.stocks import Stock
@@ -257,14 +257,43 @@ def editOffer(json=None):
 
         cheque = json['json']['offers']
 
-        cost = sum([int(row['price']) * int(row['stocks']) for row in cheque])
+        first_cost = sum([int(row['price']) * int(row['stocks']) for row in cheque])
 
         customer_wallet = db_sess.query(Wallet).filter(
             Wallet.session_id == get_session_id(),
             Wallet.user_id == current_user.id
         ).first()
 
-        if customer_wallet.money < cost:
+        company_id = get_company_id(cheque[0]['company'])
+
+        all_stocks_count = sum(map(lambda x: x[0], db_sess.query(Stock.stocks).filter(
+            Stock.session_id == get_session_id(),
+            Stock.company_id == company_id
+        ).all())) + sum(map(lambda x: x[0], db_sess.query(Offer.stocks).filter(
+            Offer.session_id == get_session_id(),
+            Offer.company_id == company_id
+        ).all()))
+
+        second_cost = 0
+        for row in cheque:
+            offer = db_sess.query(Offer).get(row['id'])
+
+            stocks_get_profit = all_stocks_count - (
+                sum(map(lambda x: x[0], db_sess.query(Stock.stocks).filter(
+                    Stock.session_id == get_session_id(),
+                    Stock.company_id == company_id,
+                    Stock.user_id.in_([current_user.id, offer.user_id])
+                ))) + sum(map(lambda x: x[0], db_sess.query(Offer.stocks).filter(
+                    Offer.session_id == get_session_id(),
+                    Offer.company_id == company_id,
+                    str(Stock.user_id).in_([current_user.id, offer.user_id])
+                )))
+            )
+
+            second_cost += int(row['price']) * int(
+                row['stocks']) * get_constant('FEE_FOR_STOCK') * stocks_get_profit
+
+        if customer_wallet.money < first_cost + second_cost:
             return send_response(
                 event_name,
                 {
@@ -273,19 +302,33 @@ def editOffer(json=None):
                 }
             )
 
-        company_id = get_company_id(cheque[0]['company'])
-
+        # Снятие денег, получение акций и начисление комиссии
         for row in cheque:
             offer = db_sess.query(Offer).get(row['id'])
 
-            customer_wallet.money -= int(row['stocks']) * int(row['price'])
+            stocks_get_profit = all_stocks_count - (
+                    sum(map(lambda x: x[0], db_sess.query(Stock.stocks).filter(
+                        Stock.session_id == get_session_id(),
+                        Stock.company_id == company_id,
+                        str(Stock.user_id).in_([current_user.id, offer.user_id])
+                    ))) + sum(map(lambda x: x[0], db_sess.query(Offer.stocks).filter(
+                        Offer.session_id == get_session_id(),
+                        Offer.company_id == company_id,
+                        str(Stock.user_id).in_([current_user.id, offer.user_id])
+                    )))
+            )
+            offer_first_cost = int(row['stocks']) * int(row['price'])
+            offer_second_cost = offer_first_cost * stocks_get_profit * get_constant('FEE_FOR_STOCK')
+
+            customer_wallet.money -= offer_first_cost + offer_second_cost
 
             seller_wallet = db_sess.query(Wallet).filter(
                 Wallet.session_id == get_session_id(),
                 Wallet.user_id == offer.user_id
             ).first()
-            seller_wallet.money += int(row['stocks']) * int(row['price'])
+            seller_wallet.money += offer_first_cost
 
+            # Изменение количества акций на торговой площадке
             if offer.stocks <= int(row['stocks']):
                 db_sess.delete(offer)
             else:
@@ -293,6 +336,7 @@ def editOffer(json=None):
                 offer.reserved_stocks -= int(row['stocks'])
                 db_sess.merge(offer)
 
+            # Получение акций в инвентарь покупателя
             stock = db_sess.query(Stock).filter(
                 Stock.session_id == get_session_id(),
                 Stock.company_id == offer.company_id,
@@ -310,6 +354,27 @@ def editOffer(json=None):
             else:
                 stock.stocks += int(row['stocks'])
                 db_sess.merge(stock)
+
+            # Начисление комиссии третьим лицам
+            all_stocks = list(map(lambda x: x[0], db_sess.query(Stock.stocks).filter(
+                Stock.session_id == get_session_id(),
+                Stock.company_id == company_id
+            ).all())) + list(map(lambda x: x[0], db_sess.query(Offer.stocks).filter(
+                Offer.session_id == get_session_id(),
+                Offer.company_id == company_id
+            ).all()))
+            for stock in all_stocks:
+
+                if str(stock.user_id) not in [current_user.id, offer.user_id]:
+                    stockholder_wallet = db_sess.query(Wallet).filter(
+                        Wallet.session_id == get_session_id(),
+                        Wallet.user_id == stock.user_id
+                    ).first()
+
+                    fee = offer_first_cost * stock.stocks * get_constant('FEE_FOR_STOCK')
+
+                    stockholder_wallet.money += fee
+                    db_sess.merge(stockholder_wallet)
 
         deleteCompanyVotes(company_id, db_sess)
         db_sess.commit()
