f3b5d927e10599af9f24b3857660f5a274cc537f
Mark Karlinsky <mark.karlinsky.04@mail.ru>
2021-08-12 12:37:50 +0500

    Implemented image uploading for company creation

--- static/js/market/create-company.js
+++ static/js/market/create-company.js
@@ -8,13 +8,50 @@
 
 companies.get(fillSectors)
 
-function createCompany() {
-    sector = document.getElementById('sector').value
-    title = document.getElementById('title').value
-    description = document.getElementById('description').value
-    logoUrl = document.getElementById('logo-url').value
+function createCompany(button) {
+    let sector = document.getElementById('sector').value
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
 
-    companies.post(sector, title, description, logoUrl, showMessage)
+    if (document.getElementById('logo').files.length === 0) {
+        companies.post(sector, title, description, null, showMessage)
+    } else {
+        let progressBar = document.getElementById("progressBar")
+        let progressBarBack = document.getElementById("progressBarBack")
+
+        let image = document.getElementById('logo').files[0]
+
+        button.disabled = true
+        progressBarBack.style.removeProperty("display")
+
+        companies.uploadImage(image, (progress) => {
+            progressBar.style.width = progress + "%"
+            progressBar.setAttribute("aria-valuemin", progress.toString())
+        }, (data) => {
+            if (data["error"]) {
+                document.getElementById('logo').classList.add('is-invalid')
+                if (data["code"] === 1001) {
+                    document.getElementById('logo-feedback').textContent = "Неверный или небезопасный формат файла"
+                }
+                progressBarBack.style.display="none"
+                progressBar.style.width = "0%"
+                progressBar.setAttribute("aria-valuemin", "0")
+                button.disabled = false
+            } else {
+                companies.post(sector, title, description, data["path"], showMessage)
+                progressBarBack.style.display="none"
+                progressBar.style.width = "0%"
+                button.disabled = false
+            }
+        }, () => {
+            document.getElementById('image-input').classList.add('is-invalid')
+            document.getElementById('image-feedback').textContent = "Файл слишком большой, пожалуйста выберите файл менее 32МБ"
+            progressBarBack.style.display="none"
+            progressBar.style.width = "0%"
+            progressBar.setAttribute("aria-valuemin", "0")
+            button.disabled = false
+        })
+    }
 }
 
 function showMessage() {
@@ -28,6 +65,8 @@ function showMessage() {
             showModal(createParagraph('Это название уже занято. Придумайте другое, ведь бренд должен быть уникальным)'))
         if (postJson['errors'][0] === 'You do not have enough money')
             showModal(createParagraph('На Вашем счёте недостаточно средств, чтобы заплатить взнос'))
+        if (postJson['errors'][0] === 'File is unsafe or located on a foreign server')
+            showModal(createParagraph('Файл небезопасный или находится на удаленном сервере'))
     }
     if (postJson['message'] === 'Success'){
         button = document.createElement('button')
@@ -50,5 +89,11 @@ function fillSectors() {
         option.value = sectors[sectorId]
         option.textContent = sectors[sectorId]
         sector.appendChild(option)
+    }
+}
+
+function valid(element) {
+    if (element.classList.contains("is-invalid")) {
+        element.classList.remove("is-invalid")
     }
 }
